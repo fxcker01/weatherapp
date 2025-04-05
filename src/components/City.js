3e252beefafdb61fe9f672d535d81ab1dfdee4fd
Andrii <andriikshnr@gmail.com>
2025-04-05 21:17:40 +0300

    edit imgs

--- src/components/City.js
+++ src/components/City.js
@@ -6,8 +6,8 @@ function City(props) {
         <h3>{props.city.name}</h3>
         <p>Temperature: {props.city.temp}</p>
         <p>Feels like: {props.city.feels}</p>
-        {props.city.temp >= 10 && <img src={sun} />}
-        {props.city.temp < 10 && <img src={clouds} />}
+        {props.city.temp >= 10 && <img src={sun} className='sun' alt="sun icon" />}
+        {props.city.temp < 10 && <img src={clouds} className='clouds' alt="clouds icon"/>}
         <button onClick={() => props.deleteWeather(props.city.name)}>Remove</button>
     </div>)
 }
